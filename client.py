1663708db6c631054d4cc6a7d190e21cf8c2b812
yisongtung <yisong@msn.com>
2017-03-24 18:16:43 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST ='' # Enter IP or Hostname of your server
+PORT = 12345 # Pick an open Port (1000+ recommended), must match the server port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#Lets loop awaiting for your input
+while True:
+    command = raw_input('Enter your command: ')
+    s.send(command)
+    reply = s.recv(1024)
+    if reply == 'Terminate':
+        break
+    print reply
